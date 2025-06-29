e76782e84192edb0575f1517e3a564ddc5de9d15
mhdhssn <mhdhssn967@gmail.com>
2025-06-29 23:30:57 +0530

    design

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,24 +6,29 @@ import './Landing.css'
 
 const Landing = ({hospName}) => {
   return (
-    <div>
+    <div className='landing-main'>
           <div className="homepage-wrapper">
             <div className="dashboard-home">
               <div className="dashboard-info">
-                <h2>Welcome, <br /> <span className="highlight"><strong>{hospName}</strong></span></h2>
+                <h2> <br /> <span className="highlight"><strong>{hospName}</strong></span></h2>
                 <p className="tagline">Monitoring VR Therapy Sessions with <strong>Happy Moves</strong></p>
                 <div className="stats-grid">
-                  <div className="stat-card">👥 Total Patients: <strong>128</strong></div>
-                  <div className="stat-card">🎮 Sessions Played: <strong>452</strong></div>
-                  <div className="stat-card">🧠 Active Today: <strong>15</strong></div>
-                  <div className="stat-card">📦 Devices Connected: <strong>6</strong></div>
-                  <div className="stat-card">⚠️ Alerts: <strong>1 Subscription Ending</strong></div>
-                  <div className="stat-card">📅 Registered Since: <strong>12/01/2024</strong></div>
+                  <div className="stat-card"><i class="fa-solid fa-bed-pulse"></i> <h4>Total Patients: <strong>128</strong></h4></div>
+                  <div className="stat-card"><i class="fa-solid fa-gamepad"></i> <h4>Sessions Played: <strong>452</strong></h4></div>
+                  <div className="stat-card"><i class="fa-solid fa-vr-cardboard"></i><h4> Devices Connected: <strong>6</strong></h4></div>
                 </div>  
               </div>
-            </div>
-              
+            </div>  
           </div>
+          <section className="quick-actions">
+        <h3>Quick Actions</h3>
+        <div className="action-buttons">
+          <button className="action-btn quick"><i class="fa-solid fa-user-plus"></i> Add New Patient</button>
+          <button className="action-btn quick"><i class="fa-solid fa-chart-simple"></i> View Reports</button>
+          <button className="action-btn quick"><i class="fa-solid fa-gear"></i>  Device Settings</button>
+          <button className='action-btn quick'><i class="fa-solid fa-headset"></i> Contact Support</button>
+        </div>
+      </section>
         </div>
   )
 }
