bf7f637da669948271aa0152f9629e8a80a201fa
mhdhssn <mhdhssn967@gmail.com>
2025-06-29 23:45:13 +0530

    design fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,7 @@ import './Landing.css'
 const Landing = ({hospName}) => {
   return (
     <div className='landing-main'>
-      <p>V 1</p>
+      <p>V 1.2</p>
           <div className="homepage-wrapper">
             <div className="dashboard-home">
               <div className="dashboard-info">
