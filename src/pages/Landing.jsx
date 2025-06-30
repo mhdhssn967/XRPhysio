a4132ec7c5aa5ebefb62c8a29a218102ba5383f1
mhdhssn <mhdhssn967@gmail.com>
2025-06-30 18:51:11 +0530

    f

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,7 @@ import './Landing.css'
 const Landing = ({hospName}) => {
   return (
     <div className='landing-main'>
-      <p>V 1.5</p>
+      <p>V 2.1</p>
           <div className="homepage-wrapper">
             <div className="dashboard-home">
               <div className="dashboard-info">
